f1352d33cdfb1fae6f54ed399ea36739f41ddc55
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-03 15:12:06 +0530

    question 2 complete

---
+++ question2.js
@@ -0,0 +1,11 @@
+// Q2. Given an array, say [1,2,3,4,5]. Print each element of an array after 3 secs.
+
+let arr = [1, 2, 3, 4, 5];
+let itr = 0;
+
+let handle = setInterval(() => {
+    console.log(arr[itr]);
+    itr++;
+    if (itr === arr.length)
+        clearInterval(handle);
+}, 1000);
