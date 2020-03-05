7a743f10969c7027fdc37b3fb443528ef9698095
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-05 14:38:12 +0530

    instance counter

--- question5.js
+++ question5.js
@@ -1,24 +1,28 @@
 // Q5. Create a function which returns number of invocations and number of instances of a function.
 
 function invocationAndInstanceCounter() {
-    if(invocationAndInstanceCounter.prototype.invocations)
+    if (invocationAndInstanceCounter.prototype.invocations)
         invocationAndInstanceCounter.prototype.invocations += 1;
     else invocationAndInstanceCounter.prototype.invocations = 1;
-    console.log("invocation count:", invocationAndInstanceCounter.prototype.invocations);
 
-    this.objectInstance = 10;
+    if (!invocationAndInstanceCounter.prototype.instances)
+        invocationAndInstanceCounter.prototype.instances = 0;
+
+    if (!(this === window))
+        invocationAndInstanceCounter.prototype.instances += 1;
+
+    console.log(
+        `invocations: ${invocationAndInstanceCounter.prototype.invocations} \t instances: ${invocationAndInstanceCounter.prototype.instances}`
+    );
 }
 
-invocationAndInstanceCounter.prototype.constructor = function() {
-    if(invocationAndInstanceCounter.prototype.instances)
-        invocationAndInstanceCounter.prototype.instances += 1;
-    else invocationAndInstanceCounter.prototype.instances = 1;
-    console.log(invocationAndInstanceCounter.prototype.instances);
-};
-
+invocationAndInstanceCounter();
 new invocationAndInstanceCounter();
-
-// for(let i = 0; i < 10; ++i)
-//     invocationAndInstanceCounter();
-
-console.log(invocationAndInstanceCounter.prototype.instances);
+invocationAndInstanceCounter();
+new invocationAndInstanceCounter();
+invocationAndInstanceCounter();
+new invocationAndInstanceCounter();
+invocationAndInstanceCounter();
+new invocationAndInstanceCounter();
+invocationAndInstanceCounter();
+new invocationAndInstanceCounter();
