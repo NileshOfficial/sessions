60786be9dd8c957ec5b106f79f17510554a7d084
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-06 13:38:16 +0530

    question 3

---
+++ question3.js
@@ -0,0 +1,40 @@
+//3. Write a program to implement inheritance upto 3 classes.The Class must  public variables and static functions.
+
+class vehicle {
+    engine = "v4";
+    wheels = 4;
+    constructor(wheels, engine) {
+        this.wheels = wheels;
+        this.engine = engine;
+    }
+}
+
+class car extends vehicle{
+    transmission = "manual";
+    constructor(wheelCount, engineType, transmission) {
+        super(wheelCount, engineType);
+        this.transmission = transmission;
+    }
+}
+
+class nissan extends car {
+    model = "GT";
+    constructor(wheelCount, engineType, transmission, model) {
+        super(wheelCount, engineType, transmission);
+        this.model = model;
+    }
+
+    getCarInfo = () => {
+        return {
+            wheels: this.wheels,
+            engine: this.engine,
+            transmission: this.transmission,
+            model: this.model
+        };
+    }
+}
+
+// let gtModel = new nissan();
+let gtrModel = new nissan(4, "v8", "automatic", "GT-R");
+// console.log(gtModel.getCarInfo());
+console.log(gtrModel.getCarInfo());
