2edd849e664eb77b237b7619fbd858903ad61cf8
NileshOfficial <nilesh.kumar@tothenew.com>
2020-03-13 12:27:32 +0530

    tweaked

--- question2.js
+++ question2.js
@@ -1,25 +1,14 @@
 //2. Find the possible combinations of a string and store them in a MAP?
 
-function getCharMap(str) {
-    let map = new Map();
-    let idx = 0;
-    for(let c of str.split("")) {
-        map.set(c, idx++);
-    }
-
-    return map;
-}
-
 function jumble(str) {
     if (str.length < 2) return str;
   
     let permutations = [];
-    let charMap = getCharMap(str);
 
     for (let i = 0; i < str.length; i++) {
       let char = str[i];
   
-      if (charMap.get(char) != i)
+      if (str.indexOf(char) != i)
         continue;
   
       let remainingString = str.slice(0, i) + str.slice(i + 1, str.length);
